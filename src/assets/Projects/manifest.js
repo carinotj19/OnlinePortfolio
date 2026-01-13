71794da8c8a7bed0612135f6f3df31794725e037
carinotj-19 <67515859+carinotj19@users.noreply.github.com>
2026-01-13 15:41:51 +0800

    New Certificate and project

--- src/assets/Projects/manifest.js
+++ src/assets/Projects/manifest.js
@@ -10,10 +10,11 @@ import simpleClock from './Simple Clock.png';
 import simpleBlackjack from './Simple Black Jack.png';
 import smartJobTracker from './Smart Job Tracker.png';
 import cabkit3D from './CabKit3D.png';
+import shelterSync from './shelter_sync.png';
 
 const projects = [
   { src: smartJobTracker, title: 'Smart Job Tracker', repo: 'https://github.com/carinotj19/job-tracker' },
-  { src: brainyBox, title: 'Brainy Box', repo: 'https://github.com/carinotj19/BrainyBox' },
+  { src: brainyBox, title: 'Brainy Box', repo: 'https://carinotj19.github.io/BrainyBox/' },
   { src: catholicSchool, title: 'Catholic School Website', demo: 'https://olmcm-website.vercel.app/' },
   { src: fineAds, title: 'Fine Ads Mockup', repo: 'https://github.com/carinotj19/Fine-Ads' },
   { src: jsDrumkit, title: 'Javascript Drumkit', repo: 'https://github.com/carinotj19/Javascript_Drumkit' },
@@ -21,7 +22,8 @@ const projects = [
   { src: simpleBlackjack, title: 'Simple Black Jack', repo: 'https://github.com/carinotj19/Blackjack' },
   { src: jeepney, title: 'Jeepney Streamlit Application', demo: 'https://jeepney-routing-system.streamlit.app/' },
   { src: responsiveWeb, title: 'Responsive Web Design', repo: 'https://github.com/carinotj19/Bootstrap-Responsive-Website' },
-  { src: cabkit3D, title: 'CabKit3D', demo: 'https://carinotj19.github.io/CabKit3D/' },
+  { src: cabkit3D, title: 'CabKit3D', repo: "",demo: 'https://carinotj19.github.io/CabKit3D/' },
+  { src: shelterSync, title: 'Shelter Sync', repo:"https://github.com/carinotj19/ShelterSync", demo: 'https://carinotj19.github.io/ShelterSync/' },
 ];
 
 export default projects;
