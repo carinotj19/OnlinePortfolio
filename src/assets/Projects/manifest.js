126e24e071ec420353729546335ce4d53cc7da8d
carinotj-19 <67515859+carinotj19@users.noreply.github.com>
2026-01-13 19:43:13 +0800

    Update Certificates, Projects and Links

--- src/assets/Projects/manifest.js
+++ src/assets/Projects/manifest.js
@@ -3,27 +3,27 @@
 import fineAds from './Fine Ads Mockup.png';
 import catholicSchool from './Catholic School Website.png';
 import brainyBox from './Brainy Box.png';
-import responsiveWeb from './Responsive web design.png';
 import jeepney from './Jeepney Streamlit Application.png';
 import jsDrumkit from './Javascript Drumkit.png';
-import simpleClock from './Simple Clock.png';
-import simpleBlackjack from './Simple Black Jack.png';
+import analogClock from './Analog Clock.png';
+import basketballScoreboard from './Basketball Scoreboard.png';
+import blackjack from './Black Jack.png';
 import smartJobTracker from './Smart Job Tracker.png';
 import cabkit3D from './CabKit3D.png';
 import shelterSync from './shelter_sync.png';
 
 const projects = [
-  { src: smartJobTracker, title: 'Smart Job Tracker', repo: 'https://github.com/carinotj19/job-tracker' },
-  { src: brainyBox, title: 'Brainy Box', repo: 'https://carinotj19.github.io/BrainyBox/' },
+  { src: cabkit3D, title: 'CabKit3D', repo: "https://github.com/carinotj19/CabKit3D",demo: 'https://carinotj19.github.io/CabKit3D/' },
+  { src: shelterSync, title: 'Shelter Sync', repo: "https://github.com/carinotj19/ShelterSync", demo: 'https://carinotj19.github.io/ShelterSync/' },
+  { src: smartJobTracker, title: 'Smart Job Tracker', repo: 'https://github.com/carinotj19/job-tracker', demo: 'https://carinotj19.github.io/job-tracker/' },
+  { src: brainyBox, title: 'Brainy Box', repo: 'https://github.com/carinotj19/BrainyBox', demo: 'https://carinotj19.github.io/BrainyBox/' },
   { src: catholicSchool, title: 'Catholic School Website', demo: 'https://olmcm-website.vercel.app/' },
-  { src: fineAds, title: 'Fine Ads Mockup', repo: 'https://github.com/carinotj19/Fine-Ads' },
-  { src: jsDrumkit, title: 'Javascript Drumkit', repo: 'https://github.com/carinotj19/Javascript_Drumkit' },
-  { src: simpleClock, title: 'Simple Clock', repo: 'https://github.com/carinotj19/CSS_JS_Clock' },
-  { src: simpleBlackjack, title: 'Simple Black Jack', repo: 'https://github.com/carinotj19/Blackjack' },
   { src: jeepney, title: 'Jeepney Streamlit Application', demo: 'https://jeepney-routing-system.streamlit.app/' },
-  { src: responsiveWeb, title: 'Responsive Web Design', repo: 'https://github.com/carinotj19/Bootstrap-Responsive-Website' },
-  { src: cabkit3D, title: 'CabKit3D', repo: "",demo: 'https://carinotj19.github.io/CabKit3D/' },
-  { src: shelterSync, title: 'Shelter Sync', repo:"https://github.com/carinotj19/ShelterSync", demo: 'https://carinotj19.github.io/ShelterSync/' },
+  { src: fineAds, title: 'Fine Ads Mockup', repo: 'https://github.com/carinotj19/Fine-Ads', demo: '' },
+  { src: jsDrumkit, title: 'Javascript Drumkit', repo: 'https://github.com/carinotj19/Javascript_Drumkit', demo: "https://carinotj19.github.io/Javascript_Drumkit/" },
+  { src: analogClock, title: 'Analog Clock', repo: 'https://github.com/carinotj19/CSS_JS_Clock', demo: 'https://carinotj19.github.io/CSS_JS_Clock/' },
+  { src: blackjack, title: 'Black Jack', repo: 'https://github.com/carinotj19/Blackjack', demo: 'https://carinotj19.github.io/Blackjack/' },
+  { src: basketballScoreboard, title: 'Basketball Scoreboard', repo: 'https://github.com/carinotj19/Basketball-scoreboard', demo: 'https://carinotj19.github.io/Basketball-scoreboard/' },
 ];
 
 export default projects;
